3bedbb695b95a441babb3c39126193f6cc4b429a
Aditya Yadav <ay5480620@gmail.com>
2025-01-26 13:42:14 +0530

    update service page

--- frontend/src/components/shared/ContentCard.tsx
+++ frontend/src/components/shared/ContentCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ROUTES } from "@/utils/constants";
+import { Link } from "react-router-dom";
 
 interface ContentCardProps {
   image: string;
@@ -6,6 +8,8 @@ interface ContentCardProps {
   heading: string;
   text: string;
   isOdd: boolean;
+  icon?: React.ReactNode;
+  buttonText?: string;
 }
 
 const ContentCard: React.FC<ContentCardProps> = ({
@@ -14,23 +18,31 @@ const ContentCard: React.FC<ContentCardProps> = ({
   heading,
   isOdd,
   imageClass,
+  icon,
+  buttonText,
 }) => {
   return (
     <div
       className={`flex ${isOdd ? "flex-row-reverse" : "flex-row"} section-width items-center justify-between`}
     >
-      <div className="flex w-1/2 justify-center overflow-hidden rounded-lg p-4">
+      <div className="flex min-h-96 flex-1 justify-center overflow-hidden rounded-lg p-4">
         <img
           src={image}
           alt="Content"
-          className={`h-auto w-full object-cover shadow-lg ${imageClass}`}
+          className={`block h-auto w-3/5 object-cover shadow-lg ${imageClass}`}
         />
       </div>
-      <div className="flex w-1/2 justify-center p-4">
-        <div className="w-5/6">
+      <div className="flex flex-1 flex-col items-center p-4">
+        <div className="mb-6">
           <h2 className="mb-4 text-4xl font-medium text-gray-800">{heading}</h2>
-          <p className="text-md text-gray-600">{text}</p>
+          <p className="text-gray-400">{text}</p>
         </div>
+        <Link
+          to={ROUTES.ABOUT}
+          className="text-md flex items-center gap-3 self-start rounded-full bg-primary px-6 py-3 py-4 text-white"
+        >
+          {buttonText} {icon && <span className="mr-2">{icon}</span>}
+        </Link>
       </div>
     </div>
   );
