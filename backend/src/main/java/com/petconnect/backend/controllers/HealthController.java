def3f30a513d19297c7dd2962535b40d08562f73
Aditya Yadav <ay5480620@gmail.com>
2025-12-16 22:25:28 +0530

    fix railway crash

---
+++ backend/src/main/java/com/petconnect/backend/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.petconnect.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
